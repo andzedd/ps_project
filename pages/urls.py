6543477972c311339840aeb1a2e1e1a0562484f4
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2019-05-23 18:19:12 -0300

    static js & fixed links

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cart', views.cart, name='cart'),
+    path('login', views.login, name='login'),
+    path('product', views.product, name='product'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,9 @@ def index(request):
 
 def cart(request):
     return render(request, 'cart.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def product(request):
+    return render(request, 'product.html')
